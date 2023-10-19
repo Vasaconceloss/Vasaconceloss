3f38f0f46fdeef118fa42fbb57e7d590df34e2df
Vasconceloss00 <148460958+Vasaconceloss@users.noreply.github.com>
2023-10-19 10:12:38 -0400

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,6 +1,7 @@
-l = input("informe o 1°valor do triângulo: ")
-l1 = input("informe o 2°valor do triângulo: ")
-l2 = input("informe o 3°valor do triângulo: ")
+l = input("informe o 1° valor do triângulo (base): ")
+l1 = input("informe o 2° valor do triângulo: ")
+l2 = input("informe o 3° valor do triângulo: ")
+h = input("informe a altura do triângulo:")
 
 if l != l1 and l != l2:
     print("O triângulo é um triângulo escaleno")
@@ -14,9 +15,13 @@ elif l1 == l and l != l2:
     print("O triângulo é um triângulo isósceles")
 elif l1 == l2 and l1 != l:
     print("O triângulo é um triângulo isósceles")
-elif l1 != l and l != l2:
+elif l1 != l and l1 != l2:
     print("O triângulo é um triângulo escaleno")
 elif l2 == l and l2 != l1:
     print("O triângulo é um triângulo isósceles")
 elif l2 == l1 and l2 != l:
     print("O triângulo é um triângulo isósceles")
+
+a = l * h / 2
+
+print("a área do triângulo é de {}" .format(a))
